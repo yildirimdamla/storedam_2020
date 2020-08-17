23ed698c299eb1b6fdaa9304e97411b02ce56cd0
Damla Yıldırım <damla.yildirim@mono.net.tr>
2020-08-17 23:42:42 +0300

    table views revised

--- src/models/product_model.ts
+++ src/models/product_model.ts
@@ -13,3 +13,59 @@ export class product_schema {
   sort: Number;
   code: String;
 }
+
+export const columnlist = [
+  {
+    sortAscending: true,
+    key: 'updatedDate',
+    value: 'Updated Date',
+  },
+  { sortAscending: true, key: 'name', value: 'Name' },
+  {
+    sortAscending: true,
+    key: 'category',
+    value: 'Category',
+  },
+  { sortAscending: true, key: 'price', value: 'Price' },
+  {
+    sortAscending: true,
+    key: 'stockNumber',
+    value: 'Stock',
+  },
+
+  {
+    sortAscending: true,
+    key: 'description',
+    value: 'Description',
+  },
+  { sortAscending: true, key: 'tag', value: 'Tag' },
+  { sortAscending: true, key: 'size', value: 'Size' },
+  { key: 'edit', value: 'Değiştir' },
+  { key: 'delete', value: 'Sil' },
+];
+export class tableoperations {
+  static sortbyString(item: any, rows) {
+    function dynamicsort(property) {
+      var sort_order = 1;
+      if (item.sortAscending == false) {
+        var sort_order = -1;
+        item.sortAscending = true;
+      } else {
+        item.sortAscending = false;
+      }
+      return function (a, b) {
+        // a should come before b in the sorted order
+        if (a[property] < b[property]) {
+          return -1 * sort_order;
+          // a should come after b in the sorted order
+        } else if (a[property] > b[property]) {
+          return 1 * sort_order;
+          // a and b are the same
+        } else {
+          return 0 * sort_order;
+        }
+      };
+    }
+    rows.sort(dynamicsort(item.key));
+  }
+}
